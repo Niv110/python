b4ba941ae8beef892cd7fbd6cb99810932a01788
Niv110 <awkash29@gmail.com>
2026-08-06 19:41:31 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,23 @@
+a=1
+print(a)
+
+b=1.2
+print(b)
+
+c="hello"
+print(c)
+
+d=True
+print(d)
+
+print("hello world \n")
+
+print("hi")
+
+print("hello",5)
+
+name=input("enter your name: ")
+print(name)
+
+age=int(input("enter your age: "))
+print(age)
