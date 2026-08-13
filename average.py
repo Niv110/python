3c7d2bc0ab16133eef770da5805e45a66a8ae51b
Niv110 <awkash29@gmail.com>
2026-08-13 19:42:15 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,13 @@
+tree1=12
+tree2=122
+tree3=1223
+tree4=373
+tree5=222
+
+sum=(tree1+tree2+tree3+tree4+tree5)
+print("sum is :",sum)
+
+total=5
+
+average=sum/total
+print("average is :",average)
---
+++ operaters.py
@@ -0,0 +1,38 @@
+#            arithimaetic operaters
+a=7
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+
+
+#            comparision operater
+
+
+print(a==b)
+print(a<b)
+print(a>b)
+print(a!=b)
+print(a<=b)
+print(a>=b)
+
+
+
+#              assignment operaters
+
+a=5
+a += 2
+print(a)
+
+a -= 3
+print(a)
+
+a *= 2
+print(a)
+
+a /= 2
+print(a)
