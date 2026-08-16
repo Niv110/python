711a773d21c9759452d335cea6511dda1ff01978
Niv110 <awkash29@gmail.com>
2026-08-16 12:41:08 +0530

    Add files via upload

---
+++ check_gr_sm.py
@@ -0,0 +1,7 @@
+number=int(input("enter your number:" ))
+
+if number >=15:
+    print("greater than 15")
+else :
+    print("lower than 15")
+
---
+++ even_odd.py
@@ -0,0 +1,6 @@
+num=int(input("print enter your number: "))
+
+if num %2==0:
+    print("number is even")
+else:
+    print("number is odd")
