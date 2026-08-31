4af14591347138a8032fb321a88b39a9cd1381ee
Niv110 <awkash29@gmail.com>
2026-08-31 16:13:17 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,23 @@
+for i in range(10):
+    print("hello")
+
+print()
+
+for i in range(10):
+    print(i)
+
+print()
+
+for i in range(1,11):
+    print(i)
+
+print()
+
+for i in range(2,11,2):
+    print("even numbers are:" ,i)
+
+
+print() 
+
+for i in range(1,11):
+    print("5 * ",i,"=",5*i)
---
+++ sum numbers.py
@@ -0,0 +1,6 @@
+n=int(input("enter your number: "))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print("sum of total number:", sum)
+
