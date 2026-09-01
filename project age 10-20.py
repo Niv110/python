0fa13b611c38003215c5aa6ffb21ab66157c0708
Niv110 <awkash29@gmail.com>
2026-09-01 17:54:45 +0530

    Add files via upload

---
+++ project age 10-20.py
@@ -0,0 +1,7 @@
+age=input("are you older than 10?(Y/N):").strip().upper()
+if age == 'Y':
+    print("are you younger than 20?(Y/N):")
+if age == 'Y':
+    print("you are eligibible")
+else:
+    print("your not eligible")
