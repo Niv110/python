f68a5b12bca52d8a836475a54636778182663369
Niv110 <awkash29@gmail.com>
2026-09-03 17:07:44 +0530

    Add files via upload

---
+++ congratulations letter.py
@@ -0,0 +1,5 @@
+congratsletter=input("congrats ram on your competition i am so happy for u i really want to know how u won from the first match to the finals in your skating competition,would u please tell me about it?(Y/N)):").strip().upper()
+if congratsletter == 'Y':
+    print("thanks u can send me back an letter!")
+else:
+    print("oh its ok.")
