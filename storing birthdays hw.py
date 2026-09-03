6c04f7105cf947016a11b5679085b8f66a06ec03
Niv110 <awkash29@gmail.com>
2026-09-03 17:18:31 +0530

    Add files via upload

---
+++ storing birthdays hw.py
@@ -0,0 +1,10 @@
+int(input("which year are u born in?: "))
+
+
+float(input("how many years are you?: "))
+
+
+str(input("what month is your birthday?: "))
+
+
+bool(input("is your birthday today?: "))
