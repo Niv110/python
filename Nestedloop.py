3bf6138bb3d0ab1f39649dcc0c2fd46241c03c57
Niv110 <awkash29@gmail.com>
2026-09-04 19:38:05 +0530

    Add files via upload

---
+++ Nestedloop.py
@@ -0,0 +1,17 @@
+i=1
+while i <= 3:
+
+    j=1
+    while j <=2:
+        print("i= ",i,"j= ",j )
+        j=j+1
+
+
+    i+=1
+
+print()
+
+
+for i in range ( 3):
+    for j in range (2):
+        print ("i= ",i,"j= ",j)
