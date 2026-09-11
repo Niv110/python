7a48a303c7b27ba1aea58037352e81459d19aecb
Niv110 <awkash29@gmail.com>
2026-09-11 19:46:17 +0530

    Add files via upload

---
+++ drawing.py
@@ -0,0 +1,30 @@
+import turtle
+
+s=turtle.Screen()
+s.bgcolor("orange")
+
+t=turtle.Turtle()
+
+for i in range(4):
+    t.forward(100)
+    t.right(90)
+
+
+
+t.circle(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
+
+
---
+++ polygon.py
@@ -0,0 +1,23 @@
+import turtle
+
+s=turtle.Screen()
+s.bgcolor("orange")
+
+t=turtle.Turtle()
+
+angle=360.0 / 3
+for i in range(3):
+    t.forward(100)
+    t.right(angle)
+
+
+
+
+
+
+
+
+
+
+
+turtle.done
