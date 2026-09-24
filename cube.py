90383036ca98643b33c24fdb54255a28415290a1
Niv110 <awkash29@gmail.com>
2026-09-24 19:40:49 +0530

    Add files via upload

---
+++ cube.py
@@ -0,0 +1,14 @@
+def cube(number):
+    return number*number*number
+
+
+def by_three(number):
+    if number % 3==0:
+        return cube(number)
+
+    else:
+        return False
+
+
+print(by_three(16))
+print(by_three(27))
